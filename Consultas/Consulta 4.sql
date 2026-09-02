d676ac5547c29d4fd5a3a4f93f6a65e77f7c85c8
Matias Hernandez <hernandez.matias.dev@gmail.com>
2026-09-02 18:43:40 -0300

    Delete Consultas/Consulta 4.sql

--- Consultas/Consulta 4.sql
+++
@@ -1,7 +0,0 @@
-USE facultad;
-SELECT persona.dni as dni, persona.nombre as nombre, persona.apellido as apellido, materia.nombre as materia, inscripcion.nota as nota
-FROM alumno
-INNER JOIN persona ON alumno.id = persona.id
-INNER JOIN inscripcion ON inscripcion.alumno_id = alumno.id
-INNER JOIN 	materia ON materia.id  = inscripcion.materia_id
-WHERE nota IS NOT NULL;
