c9f92b5dc8e3f55185fc51b2c13c03eb012f4cc3
Matias Hernandez <hernandez.matias.dev@gmail.com>
2026-09-02 18:46:58 -0300

    Add files via upload

---
+++ first_INSERT.sql
@@ -0,0 +1,20 @@
+
+INSERT ciudad (nombre)
+VALUE ("Buenos Aires");
+
+INSERT departamento (nombre)
+VALUE ("Ingenieria UNAJ");
+
+INSERT titulo (nombre, institucion)
+VALUE ("Ingeniero en infomratica","UNAJ");
+
+INSERT persona (dni, nombre, apellido, fecha_nacimiento)
+VALUE (40234321, "Pedro","Perez","1999-02-11");
+
+INSERT profesor (id, cuil, departamento_id, ciudad_nacimiento_id)
+VALUE  (1,20402343219,1,1);
+
+
+
+
+
