0a468ec539b35e810adedc27ed71b40efb41cec8
Matias Hernandez <hernandez.matias.dev@gmail.com>
2026-09-02 18:43:23 -0300

    Delete Consultas/Consulta 2.sql

--- Consultas/Consulta 2.sql
+++
@@ -1,6 +0,0 @@
-USE facultad;
-SELECT * 
-FROM alumno
-INNER JOIN persona ON alumno.id = persona.id
-INNER JOIN inscripcion ON inscripcion.alumno_id = alumno.id
-INNER JOIN 	materia ON materia.id  = inscripcion.materia_id;
